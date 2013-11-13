1fb435e05a61750ce1f00b5f042b99b4fbabe5ea
Christopher Duarte <cjduar@gmail.com>
2013-11-12 21:06:19 -0800

    Initialize repository

---
+++ hambangeles/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hambangeles::Application.config.secret_token = '03f91d1c8c9cfe791a2205b2b430a62460733471465091e2efdb9bec00a3c58990f7a384aee08a3ce300189b054bab5d60a5c0fe10d1361d82f13ab5db422ff2'
